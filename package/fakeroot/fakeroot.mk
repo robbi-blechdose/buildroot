e4cb60c808e8e12c1bbceae18bcfe0095b524948
Ilya Mizus <bigfatbrowncat@gmail.com>
2024-02-18 15:13:17 +0000

    Fixed build on Ubuntu 22.04.3

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-FAKEROOT_VERSION = 1.25.3
+FAKEROOT_VERSION = 1.26
 FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.gz
-FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20201008T205817Z/pool/main/f/fakeroot
+#FAKEROOT_SITE = https://snapshot.debian.org/archive/debian/20201008T205817Z/pool/main/f/fakeroot
+FAKEROOT_SITE = https://src.fedoraproject.org/repo/pkgs/fakeroot/fakeroot_1.26.orig.tar.gz/sha512/dad193b283b48a25806c5bcd06d1f5bbd183ccd40a3fc25f3e7bb88fd6579024f958321f8336654348a1d0a547d4ad19ed11aab5ec9441d6ef493ee6e4c78081/
 
 HOST_FAKEROOT_DEPENDENCIES = host-acl
 # Force capabilities detection off
